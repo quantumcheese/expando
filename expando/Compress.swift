2106430a41f47e0477bd51d480ebbab340ae8902
Richard A. Brown <rabrown@quantumcheese.com>
2017-01-02 13:29:33 -0500

    Count consecutive equal bits from current position.

--- expando/Compress.swift
+++ expando/Compress.swift
@@ -8,8 +8,86 @@
 
 import Foundation
 
+typealias DataCounter = (Data) -> Int
+typealias BytePredicate = (UInt8) -> Bool
+
+/*
+fileprivate func generateInitialCounter(_ stopPredicate: @escaping BytePredicate) -> DataCounter {
+    return { (_ data: Data) -> Int in
+        var count = 0
+
+        data.forEach({(_ byte: UInt8) -> () in
+            for i in (0..<8).reversed() {
+                let bitmask = (UInt8(1) << UInt8(i))
+                let maskedByte = byte & bitmask
+                if (stopPredicate(maskedByte)) {
+                    break
+                }
+                count += 1
+            }
+        })
+        return count
+    }
+}
+
+ /*
+ let countInitialZeros = generateInitialCounter({byte in return 0 != byte})
+ let countInitialOnes = generateInitialCounter({byte in return 0 == byte})
+ */
+ */
+let ONE = UInt8(1)
+let MAX_NUMBER_OF_BITS = UInt8(8)
 class Compress {
 
+    enum BoundaryConditions : Error {
+        case emptyData()
+        case byteIndexTooHigh(index:Int)
+        case bitIndexTooHigh(index: UInt8)
+    }
+
+    func startsWithZero(bytes: [UInt8]) throws -> Bool {
+        if (0 == bytes.count) {
+            throw BoundaryConditions.emptyData()
+        }
+
+        return 0 == (bytes[0] & ONE)
+    }
+
+    func countFromPosition(bytes: [UInt8], byteIndex startingByteIndex: Int, bitIndex startingBitIndex: UInt8) throws -> Int {
+        if (0 == bytes.count) {
+            throw BoundaryConditions.emptyData()
+        }
+        if (bytes.count <= startingByteIndex) {
+            throw BoundaryConditions.byteIndexTooHigh(index: startingByteIndex)
+        }
+        if (MAX_NUMBER_OF_BITS <= startingBitIndex) {
+            throw BoundaryConditions.bitIndexTooHigh(index: startingBitIndex)
+        }
+
+        var byteIndex = startingByteIndex
+        var bitIndex = startingBitIndex
+        // get the bit at the current position
+        var byte = bytes[byteIndex]
+        let bitmask = (byte >> bitIndex) & ONE
+
+        var count = 0
+        while (bitmask == ((byte >> bitIndex) & ONE)) {
+            count += 1
+            bitIndex += 1
+            if (MAX_NUMBER_OF_BITS == bitIndex) {
+                bitIndex = 0
+                byteIndex += 1
+                if (byteIndex == bytes.count) {
+                    break
+                }
+                byte = bytes[byteIndex]
+            }
+        }
+
+        return count
+    }
+
+/*
     func countInitialZeros(_ data: Data) -> Int {
         var count = 0
 
@@ -25,4 +103,5 @@ class Compress {
         })
         return count
     }
+ */
 }
--- test/CompressTest.swift
+++ test/CompressTest.swift
@@ -22,22 +22,102 @@ class CompressTest: XCTestCase {
         super.tearDown()
     }
 
-    func testZeros() {
-        var binary: [UInt8] = [0b00000000]
-        var data = Data(bytes: binary)
-        var count = compressor!.countInitialZeros(data)
+    func testCountZeros() {
+        var binary = [UInt8.allZeros]
+        var count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 0)
         XCTAssertEqual(count, 8)
 
-        binary = [0b01000000]
-        data = Data(bytes: binary)
-        count = compressor!.countInitialZeros(data)
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 3)
+        XCTAssertEqual(count, 5)
+
+
+        binary = [0b00000010, UInt8.allZeros]
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 0)
         XCTAssertEqual(count, 1)
 
-        binary = [0b10000000]
-        data = Data(bytes: binary)
-        count = compressor!.countInitialZeros(data)
-        XCTAssertEqual(count, 0)
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 2)
+        XCTAssertEqual(count, 14)
+    }
 
+    func testCountOnes() {
+        var binary = [UInt8.max]
+        var count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 0)
+        XCTAssertEqual(count, 8)
+
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 3)
+        XCTAssertEqual(count, 5)
+
+
+        binary = [0b11111101, UInt8.max]
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 0)
+        XCTAssertEqual(count, 1)
+
+        count = try! compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 2)
+        XCTAssertEqual(count, 14)
+    }
+
+    func testEmptyBytes() {
+        let binary: Array<UInt8> = []
+        do {
+            _ = try compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 0)
+        } catch Compress.BoundaryConditions.emptyData() {
+            // expected exception
+        } catch let e {
+            XCTFail("Caught unexpected exception \(e)")
+        }
+    }
+
+    func testEndOfBytes() {
+        let binary = [UInt8.max]
+        do {
+            _ = try compressor!.countFromPosition(bytes: binary, byteIndex: 1, bitIndex: 0)
+        } catch Compress.BoundaryConditions.byteIndexTooHigh(index: let e) {
+            XCTAssertEqual(e, 1)
+        } catch let e {
+            XCTFail("Caught unexpected exception \(e)")
+        }
+    }
+
+    func testTooManyBits() {
+        let binary = [UInt8.allZeros]
+        do {
+            _ = try compressor!.countFromPosition(bytes: binary, byteIndex: 0, bitIndex: 43)
+        } catch Compress.BoundaryConditions.bitIndexTooHigh(index: let e) {
+            XCTAssertEqual(e, 43)
+        } catch let e {
+            XCTFail("Caught unexpected exception \(e)")
+        }
+    }
+
+    func testStartsWithZero() {
+        var binary = [UInt8.allZeros]
+        var startsWithZero = try! compressor!.startsWithZero(bytes: binary)
+        XCTAssertTrue(startsWithZero)
+
+        binary = [0b10000000, UInt8.max]
+        startsWithZero = try! compressor!.startsWithZero(bytes: binary)
+        XCTAssertTrue(startsWithZero)
+    }
+
+    func testNotStartWithZero() {
+        var binary = [UInt8.max]
+        var startsWithZero = try! compressor!.startsWithZero(bytes: binary)
+        XCTAssertFalse(startsWithZero)
+
+        binary = [0b000000001, UInt8.allZeros]
+        startsWithZero = try! compressor!.startsWithZero(bytes: binary)
+        XCTAssertFalse(startsWithZero)
+    }
+
+    func testEmptyDataExceptsWithZero() {
+        let binary: Array<UInt8> = []
+        do {
+            _ = try compressor!.startsWithZero(bytes: binary)
+        } catch Compress.BoundaryConditions.emptyData() {
+            // expected exception
+        } catch let e {
+            XCTFail("Caught unexpected exception \(e)")
+        }
     }
 
  /*   func testPerformanceExample() {
