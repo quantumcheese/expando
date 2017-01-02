a0574b0183fa341e1d05ab9cfad400c6ca85d4e2
Richard A. Brown <rabrown@quantumcheese.com>
2017-01-02 09:42:05 -0500

    Initial Commit

---
+++ expando/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  expando
+//
+//  Created by Yosef Brown on 2017-01-02.
+//  Copyright © 2017 Quantum Cheese Coding, LLC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
