f87d55af14fa4f70a8790bf6dd81f7bd9b4f8d94
Richard A. Brown <rabrown@quantumcheese.com>
2017-04-14 15:17:55 -0400

    Only upgrade swiftlint if needed.

--- scripts/install_swiftlint.sh
+++ scripts/install_swiftlint.sh
@@ -2,7 +2,14 @@
 set -e
 set -x
 
+function is_upgradeable {
+  return $(brew outdated) | grep "swiftlint"
+}
+
 if which swiftlint >/dev/null; then
+  if ! is_upgradeable ; then
+    exit 0
+  fi
   COMMAND="upgrade"
 else
   COMMAND="install"
