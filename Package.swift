137491760acaa9c78b3b5634791050180e0c9437
Richard A. Brown <rabrown@quantumcheese.com>
2017-04-23 23:08:10 -0400

    Depend on CommandLine kit.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Inflato"
+  name: "Inflato",
+  dependencies: [
+    .Package(url: "https://github.com/jatoben/CommandLine", Version("3.0.0-pre1"))
+  ]
 )
